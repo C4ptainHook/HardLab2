896682f112595b015ca8311d01a0049928457867
C4ptainHook <boykostudyinkpi@gmail.com>
2023-05-13 23:04:28 +0300

    Deleted ctr from Counter + added new method

--- Counter.h
+++ Counter.h
@@ -5,10 +5,11 @@
 class Counter {
     int accesable_subj;
     std::vector<Student> data;
-    Counter(const std::vector<Student>&);
     void Count_averSubj();
     void Count_averScore();
     void Sort_data();
+public:
+    void Process_parsedData(const std::vector<Student>&);
 };
 
 
