1a9e5ad649b35d5a68d220adf9c7159baf23df09
C4ptainHook <boykostudyinkpi@gmail.com>
2023-05-13 13:07:17 +0300

    Add new function for dir processing to Directory.h

--- Directory.h
+++ Directory.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include <fstream>
 #include <filesystem>
 #include <vector>
 #include <string>
@@ -14,5 +15,21 @@ namespace DirSearch {
             path_bundle.push_back(entry.path().string());
         }
         return path_bundle;
+    }
+
+    std::string DirProcess(const std::vector<std::string> &_path_bundle, const char* path)
+    {
+        std::string converter = path;
+        converter+="\\Parser_Folder";
+        std::filesystem::create_directory(converter);
+        converter+="\\Newfile.csv";
+        for(const auto& elem : _path_bundle)
+        {
+            std::ifstream read_file(elem);
+            std::ofstream write_file;
+            write_file.open(converter, std::ios::out | std::ios::app);
+
+
+        }
     }
 }
