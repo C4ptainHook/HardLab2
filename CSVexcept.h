0b5d8232d3e4a25801f48503b7ff50b5d2093b81
C4ptainHook <boykostudyinkpi@gmail.com>
2023-05-16 09:24:50 +0300

    Class CSV exception added

---
+++ CSVexcept.h
@@ -0,0 +1,26 @@
+#ifndef INC_2_PARSER__CSVEXCEPT_H
+#define INC_2_PARSER__CSVEXCEPT_H
+#include <string>
+#include <stdexcept>
+
+struct FileErr{
+    std::string filename;
+    int col_num=0;
+};
+
+class FileContentException: public std::exception{
+    FileErr err;
+public:
+    FileContentException(const std::string& _fName, int _c)
+    {
+        err.filename=_fName;
+        err.col_num=_c;;
+    }
+    const std::string whatHappened () const{
+        std::string subj = "Error in "+err.filename;
+        subj+= " in coloumn:"+std::to_string(err.col_num);
+        return subj;
+    }
+};
+
+#endif //INC_2_PARSER__CSVEXCEPT_H
